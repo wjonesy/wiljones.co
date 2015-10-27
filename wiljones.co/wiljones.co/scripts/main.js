d86c9a2a41ccaf3723599d44032226a5089f97dd
wjonesy <wtrjones@gmail.com>
2015-10-27 00:28:42 +0000

    finally some simple progress

---
+++ wiljones.co/wiljones.co/scripts/main.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+  
+
+ 
+
+    $('.js-skew-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsive: {
+            0: {
+                items: 6
+            }
+        }
+    });
+
+});
